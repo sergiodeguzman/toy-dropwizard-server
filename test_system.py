30fa3e533bc511ac84c7a29e0ceba85d47fe5853
ahalyajupalli <ahalya.jupalli@gmail.com>
2024-01-31 15:47:00 -0700

    changed

--- test_system.py
+++ test_system.py
@@ -4,11 +4,11 @@ import requests
 def test_code():
     response=requests.get("http://localhost:8085")
     json_response=response.json()
-    assert(json_request["code"]==404)
+    assert(json_response["code"]==404)
 def test_message():
     response=requests.get("http://localhost:8085")
     json_response=response.json()
-    assert(json_request["message"]=="HTTP 404 Not Found")
+    assert(json_response["message"]=="HTTP 404 Not Found")
 
 
 
